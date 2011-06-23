d9a35506b99775f3d5a753dc502b15896b082247
Jay Luker <jluker@cfa.harvard.edu>
2011-06-23 12:27:13 -0400

    added target method for bulk update

--- src/python/montysolr/adslabs/targets.py
+++ src/python/montysolr/adslabs/targets.py
@@ -5,6 +5,7 @@ from montysolr.initvm import montysolr_java as sj
 from montysolr.utils import MontySolrTarget
 import logging
 import os
+import montysolr.adslabs.api_calls as api_calls
 #import montysolr.adslabs.multiprocess_api_calls as api_calls
 
 import time
@@ -34,8 +35,8 @@ def workout_field_value(message):
                     docs = mongo['solr4ads']['docs']
                     bib = vals['bibcode']
                     doc = docs.find_one({'bibcode': bib}, {'body': 1})
-                    if doc:
-                        message.setResults(doc['body'])
+                    if doc and doc.has_key('body'):
+                        message.setResults(doc.get('body', ""))
                         return
                 else:
                     dirs = vals['src_dir'].split(',')
@@ -50,7 +51,22 @@ def workout_field_value(message):
                             ret = fo.read()
                             message.setResults(ret.decode('utf-8'))
                             return
-
+                        
+def get_recids_changes(message):
+    """Retrieves the recids of the last changed documents"""
+    last_recid = int(sj.Integer.cast_(message.getParam("last_recid")).intValue())
+    max_records = 10000
+    if message.getParam('max_records'):
+        mr = int(sj.Integer.cast_(message.getParam("max_records")).intValue())
+        if mr < 100001:
+            max_records = mr
+    (wid, results) = api_calls.dispatch("get_recids_changes", last_recid, max_records)
+    if results:
+        out = sj.HashMap().of_(sj.String, sj.JArray_int)
+        for k,v in results.items():
+            out.put(k, sj.JArray_int(v))
+        message.setResults(out)
+        
 def montysolr_targets():
     targets = [
            MontySolrTarget('PythonTextField:workout_field_value', workout_field_value),
@@ -60,7 +76,7 @@ def montysolr_targets():
 #           MontySolrTarget('InvenioQuery:perform_request_search_ints', perform_request_search_ints),
 #           MontySolrTarget('InvenioQuery:perform_request_search_bitset', perform_request_search_bitset),
 #           MontySolrTarget('InvenioFormatter:format_search_results', format_search_results),
-#           MontySolrTarget('InvenioKeepRecidUpdated:get_recids_changes', get_recids_changes),
+           MontySolrTarget('InvenioKeepRecidUpdated:get_recids_changes', get_recids_changes),
 #           MontySolrTarget('InvenioFormatter:sort_and_format', sort_and_format),
 #           MontySolrTarget('Invenio:diagnostic_test', diagnostic_test),
            ]
