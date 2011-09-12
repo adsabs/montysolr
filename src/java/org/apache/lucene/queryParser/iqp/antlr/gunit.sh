007dd9d6082e6c0973666b5a6cdcad2e807ecb83
rchyla <roman.chyla@gmail.com>
2011-09-12 20:50:28 +0200

    adding unittests for the grammar

--- src/java/org/apache/lucene/queryParser/iqp/antlr/gunit.sh
+++ src/java/org/apache/lucene/queryParser/iqp/antlr/gunit.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-echo 'Input is:' $1
 
 CP=.:/dvt/antlr-142/lib/antlr-3.4-complete.jar:/x/dev/antlr-34/lib/antlr-3.4-complete.jar
 
-rm ast-tree.dot
+
 java -cp $CP org.antlr.Tool StandardLuceneGrammar.g
 javac -cp $CP *.java
-java -cp $CP Demo "$1" > ast-tree.dot
 
-XDOT=`which xdot`
-if [ -f $XDOT ];
-then
-  $XDOT ast-tree.dot 
-else
-  open -a graphviz ast-tree.dot
-fi
+java -cp $CP org.antlr.gunit.Interp StandardLuceneGrammar.gunit 
+
+
