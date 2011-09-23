2dc12d9927907881d357f003e6582dd9d77bbd37
rchyla <roman.chyla@gmail.com>
2011-09-23 19:58:35 +0200

    Also fixed the gunit suite

--- src/java/org/apache/lucene/queryParser/iqp/antlr/gunit.sh
+++ src/java/org/apache/lucene/queryParser/iqp/antlr/gunit.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 
-CP=.:/dvt/antlr-142/lib/antlr-3.4-complete.jar:/x/dev/antlr-34/lib/antlr-3.4-complete.jar
+CP=.:/dvt/antlr-142/lib/antlr-3.4-complete.jar:/x/dev/antlr-34/lib/antlr-3.4-complete.jar:../../../../../..
 
 
 java -cp $CP org.antlr.Tool StandardLuceneGrammar.g
 javac -cp $CP *.java
 
 java -cp $CP org.antlr.gunit.Interp StandardLuceneGrammar.gunit 
-
 
