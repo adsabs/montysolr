963bcfb86d3326bf8b5e4736fffc69cfb6da675d
rca <rca@squirrel.(none)>
2011-10-21 11:48:38 +0200

    Simpler unittest

---
+++ test/java/org/apache/lucene/queryParser/aqp/TestQPHelperAqpSimple.java
@@ -0,0 +1,288 @@
+package org.apache.lucene.queryParser.aqp;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.text.Collator;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Collections;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.KeywordAnalyzer;
+import org.apache.lucene.analysis.LowerCaseTokenizer;
+import org.apache.lucene.analysis.SimpleAnalyzer;
+import org.apache.lucene.analysis.StopAnalyzer;
+import org.apache.lucene.analysis.StopFilter;
+import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
+import org.apache.lucene.document.DateField;
+import org.apache.lucene.document.DateTools;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.messages.MessageImpl;
+import org.apache.lucene.queryParser.core.QueryNodeException;
+import org.apache.lucene.queryParser.core.messages.QueryParserMessages;
+import org.apache.lucene.queryParser.core.nodes.FuzzyQueryNode;
+import org.apache.lucene.queryParser.core.nodes.QueryNode;
+import org.apache.lucene.queryParser.core.processors.QueryNodeProcessorImpl;
+import org.apache.lucene.queryParser.core.processors.QueryNodeProcessorPipeline;
+import org.apache.lucene.queryParser.standard.QueryParserUtil;
+import org.apache.lucene.queryParser.standard.config.DefaultOperatorAttribute.Operator;
+import org.apache.lucene.queryParser.standard.nodes.WildcardQueryNode;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.FuzzyQuery;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.MultiPhraseQuery;
+import org.apache.lucene.search.MultiTermQuery;
+import org.apache.lucene.search.PhraseQuery;
+import org.apache.lucene.search.PrefixQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TermRangeQuery;
+import org.apache.lucene.search.WildcardQuery;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.util.LuceneTestCase;
+
+/**
+ * This test case is a copy of the core Lucene query parser test, it was adapted
+ * to use new QueryParserHelper instead of the old query parser.
+ * 
+ * Tests QueryParser.
+ */
+public class TestQPHelperAqpSimple extends LuceneTestCase {
+
+	public static Analyzer qpAnalyzer = new QPTestAnalyzer();
+
+	public static final class QPTestFilter extends TokenFilter {
+		private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
+		private final OffsetAttribute offsetAtt = addAttribute(OffsetAttribute.class);
+
+		/**
+		 * Filter which discards the token 'stop' and which expands the token
+		 * 'phrase' into 'phrase1 phrase2'
+		 */
+		public QPTestFilter(TokenStream in) {
+			super(in);
+		}
+
+		boolean inPhrase = false;
+		int savedStart = 0, savedEnd = 0;
+
+		@Override
+		public boolean incrementToken() throws IOException {
+			if (inPhrase) {
+				inPhrase = false;
+				clearAttributes();
+				termAtt.setEmpty().append("phrase2");
+				offsetAtt.setOffset(savedStart, savedEnd);
+				return true;
+			} else
+				while (input.incrementToken()) {
+					if (termAtt.toString().equals("phrase")) {
+						inPhrase = true;
+						savedStart = offsetAtt.startOffset();
+						savedEnd = offsetAtt.endOffset();
+						termAtt.setEmpty().append("phrase1");
+						offsetAtt.setOffset(savedStart, savedEnd);
+						return true;
+					} else if (!termAtt.toString().equals("stop"))
+						return true;
+				}
+			return false;
+		}
+	}
+
+	public static final class QPTestAnalyzer extends Analyzer {
+
+		/** Filters LowerCaseTokenizer with StopFilter. */
+		@Override
+		public final TokenStream tokenStream(String fieldName, Reader reader) {
+			return new QPTestFilter(new LowerCaseTokenizer(
+					TEST_VERSION_CURRENT, reader));
+		}
+	}
+
+	public static class QPTestParser extends ANTLRQueryParser {
+		public QPTestParser(Analyzer a) {
+			((QueryNodeProcessorPipeline) getQueryNodeProcessor())
+					.add(new QPTestParserQueryNodeProcessor());
+			this.setAnalyzer(a);
+
+		}
+
+		private static class QPTestParserQueryNodeProcessor extends
+				QueryNodeProcessorImpl {
+
+			@Override
+			protected QueryNode postProcessNode(QueryNode node)
+					throws QueryNodeException {
+
+				return node;
+
+			}
+
+			@Override
+			protected QueryNode preProcessNode(QueryNode node)
+					throws QueryNodeException {
+
+				if (node instanceof WildcardQueryNode
+						|| node instanceof FuzzyQueryNode) {
+
+					throw new QueryNodeException(new MessageImpl(
+							QueryParserMessages.EMPTY_MESSAGE));
+
+				}
+
+				return node;
+
+			}
+
+			@Override
+			protected List<QueryNode> setChildrenOrder(List<QueryNode> children)
+					throws QueryNodeException {
+
+				return children;
+
+			}
+
+		}
+
+	}
+
+	private int originalMaxClauses;
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		originalMaxClauses = BooleanQuery.getMaxClauseCount();
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		BooleanQuery.setMaxClauseCount(originalMaxClauses);
+		super.tearDown();
+	}
+
+	
+	public ANTLRQueryParser getParser(Analyzer a) throws Exception {
+		if (a == null)
+			a = new SimpleAnalyzer(TEST_VERSION_CURRENT);
+		ANTLRQueryParser qp = new ANTLRQueryParser();
+		qp.setAnalyzer(a);
+		return qp;
+	}
+
+	public Query getQuery(String query, Analyzer a) throws Exception {
+		return getParser(a).parse(query, "field");
+	}
+
+
+	public void assertQueryEquals(String query, Analyzer a, String result)
+			throws Exception {
+		Query q = getQuery(query, a);
+		String s = q.toString("field");
+		if (!s.equals(result)) {
+			fail("Query /" + query + "/ yielded /" + s + "/, expecting /"
+					+ result + "/");
+		}
+	}
+
+
+	public void assertQueryEquals(ANTLRQueryParser qp, String field,
+			String query, String result) throws Exception {
+		Query q = qp.parse(query, field);
+		String s = q.toString(field);
+		if (!s.equals(result)) {
+			fail("Query /" + query + "/ yielded /" + s + "/, expecting /"
+					+ result + "/");
+		}
+	}
+
+	public void assertEscapedQueryEquals(String query, Analyzer a, String result)
+			throws Exception {
+		String escapedQuery = QueryParserUtil.escape(query);
+		if (!escapedQuery.equals(result)) {
+			fail("Query /" + query + "/ yielded /" + escapedQuery
+					+ "/, expecting /" + result + "/");
+		}
+	}
+
+
+
+
+	public void testBooleanQuery() throws Exception {
+		
+		WhitespaceAnalyzer analyzer = new WhitespaceAnalyzer(TEST_VERSION_CURRENT);
+		
+		ANTLRQueryParser qp = getParser(analyzer);
+		
+		Query query = qp.parse("this OR that AND thus", "field");
+		
+		assertQueryEquals(query.toString(), analyzer, "BooleanQuery()");
+		
+		BooleanQuery.setMaxClauseCount(2);
+		try {
+			qp = getParser(new WhitespaceAnalyzer(TEST_VERSION_CURRENT));
+
+			qp.parse("one two three", "field");
+			fail("ParseException expected due to too many boolean clauses");
+		} catch (QueryNodeException expected) {
+			// too many boolean clauses, so ParseException is expected
+		}
+	}
+
+
+	private class CannedTokenStream extends TokenStream {
+		private int upto = 0;
+		final PositionIncrementAttribute posIncr = addAttribute(PositionIncrementAttribute.class);
+		final CharTermAttribute term = addAttribute(CharTermAttribute.class);
+
+		@Override
+		public boolean incrementToken() {
+			clearAttributes();
+			if (upto == 4) {
+				return false;
+			}
+			if (upto == 0) {
+				posIncr.setPositionIncrement(1);
+				term.setEmpty().append("a");
+			} else if (upto == 1) {
+				posIncr.setPositionIncrement(1);
+				term.setEmpty().append("b");
+			} else if (upto == 2) {
+				posIncr.setPositionIncrement(0);
+				term.setEmpty().append("c");
+			} else {
+				posIncr.setPositionIncrement(0);
+				term.setEmpty().append("d");
+			}
+			upto++;
+			return true;
+		}
+	}
+
+	private class CannedAnalyzer extends Analyzer {
+		@Override
+		public TokenStream tokenStream(String ignored, Reader alsoIgnored) {
+			return new CannedTokenStream();
+		}
+	}
+
+}
