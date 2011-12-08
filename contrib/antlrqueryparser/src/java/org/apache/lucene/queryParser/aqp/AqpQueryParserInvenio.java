ee93739cbf9f540da3c9259094f456b828527df0
rchyla <roman.chyla@gmail.com>
2011-12-09 00:36:54 +0100

    Completed the grammar for Invenio; present are classes that handle everything, but i have to adjust the knobs -- and play with the new qp processors. TODO: move the grammars into the central folder? Move InvenioQParser into contrib

---
+++ contrib/antlrqueryparser/src/java/org/apache/lucene/queryParser/aqp/AqpQueryParserInvenio.java
@@ -0,0 +1,21 @@
+package org.apache.lucene.queryParser.aqp;
+
+import org.apache.lucene.queryParser.aqp.builders.AqpStandardQueryTreeBuilder;
+import org.apache.lucene.queryParser.aqp.config.AqpStandardQueryConfigHandler;
+import org.apache.lucene.queryParser.aqp.parser.AqpSyntaxParserInvenio;
+import org.apache.lucene.queryParser.aqp.processors.AqpQueryNodeProcessorPipeline;
+
+public class AqpQueryParserInvenio extends AqpQueryParser {
+
+	public AqpQueryParserInvenio() throws Exception {
+		super(new AqpStandardQueryConfigHandler(), 
+				new AqpSyntaxParserInvenio(),
+				new AqpQueryNodeProcessorPipeline(null),
+				new AqpStandardQueryTreeBuilder());
+	}
+	
+	public AqpQueryParserInvenio(String grammarName) throws Exception {
+		throw new IllegalArgumentException("Invenio query parser does not use reflection");
+	}
+	
+}
