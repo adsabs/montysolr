2691addea5b026461d0f8fed4383aa35d4637d55
Roman Chyla <roman.chyla@gmail.com>
2012-03-30 21:24:49 -0400

    Adding classpath, otherwise examples are not found

--- contrib/examples/src/test/examples/adslabs/BlackBoxADSIndexing.java
+++ contrib/examples/src/test/examples/adslabs/BlackBoxADSIndexing.java
@@ -1,8 +1,14 @@
 package examples.adslabs;
 
+
 public class BlackBoxADSIndexing extends BlackBoxADSAbstract{
 	
 	public void testPython() {
 		System.out.println("hi!");
 	}
+	
+	// Uniquely for Junit 3
+	public static junit.framework.Test suite() {
+        return new junit.framework.JUnit4TestAdapter(BlackBoxADSIndexing.class);
+    }
 }
