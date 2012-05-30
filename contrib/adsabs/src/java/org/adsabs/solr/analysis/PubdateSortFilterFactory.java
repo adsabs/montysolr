18fa68b6937413329c97f6a88e0b6d505fc5b6a9
Jay Luker <jluker@cfa.harvard.edu>
2012-05-30 16:36:31 -0400

    removed use of script transformers in data-config and reimpl functionality in token filters; added several fields, removed a few others

---
+++ contrib/adsabs/src/java/org/adsabs/solr/analysis/PubdateSortFilterFactory.java
@@ -0,0 +1,13 @@
+package org.adsabs.solr.analysis;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.solr.analysis.BaseTokenFilterFactory;
+
+public class PubdateSortFilterFactory extends BaseTokenFilterFactory {
+
+	@Override
+	public TokenStream create(TokenStream input) {
+		return new PubdateSortFilter(input);
+	}
+
+}
