e9f0ebfef88450ee686fd0c2fd5549cf843396b3
Roman Chyla <roman.chyla@gmail.com>
2012-06-05 13:08:31 -0400

    bibstem now contains both journal name and journal.volume

--- contrib/adsabs/src/java/org/adsabs/solr/analysis/BibstemFilterFactory.java
+++ contrib/adsabs/src/java/org/adsabs/solr/analysis/BibstemFilterFactory.java
@@ -1,12 +1,23 @@
 package org.adsabs.solr.analysis;
 
+import java.util.Map;
+
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.solr.analysis.BaseTokenFilterFactory;
 
 public class BibstemFilterFactory extends BaseTokenFilterFactory {
+	
+	private boolean facetMode = false;
+	
+	public void init(Map<String,String> args) {
+		String f = args.get("facetMode");
+	    if (f != null && f.toLowerCase().contains("true")) {
+	      facetMode = true;
+	    }
+	}
 
 	public TokenStream create(TokenStream input) {
-		return new BibstemFilter(input);
+		return new BibstemFilter(input, facetMode);
 	}
 
 }
