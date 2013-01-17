cbee89a4bab62725b0fbfe11608a6c7162742b12
Roman Chyla <roman.chyla@gmail.com>
2013-01-17 18:22:58 -0500

    2nd order test failing in Hudson - huh???

--- contrib/invenio/src/test/org/apache/lucene/search/TestSecondOrderQueryTypesAds.java
+++ contrib/invenio/src/test/org/apache/lucene/search/TestSecondOrderQueryTypesAds.java
@@ -2,7 +2,6 @@ package org.apache.lucene.search;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
