c55edba71fbec0b3fb377c4408c81273258240b8
Roman Chyla <roman.chyla@gmail.com>
2013-10-04 21:14:17 -0400

    Hmmm. worrying that multiprocessing causes core dumps...

--- src/python/montysolr/config.py
+++ src/python/montysolr/config.py
@@ -12,7 +12,7 @@ MONTYSOLR_HANDLER = 'montysolr.sequential_handler'
 MONTYSOLR_TARGETS = [] #'montysolr.inveniopie.targets', 'montysolr.examples.twitter_test']
 
 # use the multiprocessing version of api_calls, value -1 means: detect number of cpus
-MONTYSOLR_MAX_WORKERS = 4
+MONTYSOLR_MAX_WORKERS = 0
 
 # will be appended to jvm args when starting the virtual machine (Java inside Python)
 MONTYSOLR_JVMARGS_PYTHON = ''
