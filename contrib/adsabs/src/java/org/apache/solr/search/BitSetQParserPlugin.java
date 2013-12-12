2ddfda0645f19293f8d01d88d106d8a7cefdc030
Roman Chyla <roman.chyla@gmail.com>
2013-12-12 14:06:08 -0500

    Moving commit...

--- contrib/adsabs/src/java/org/apache/solr/search/BitSetQParserPlugin.java
+++ contrib/adsabs/src/java/org/apache/solr/search/BitSetQParserPlugin.java
@@ -28,6 +28,17 @@ import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.schema.SchemaField;
 import org.apache.solr.update.processor.UpdateRequestProcessor;
 
+/*
+ * Implementation note: I wanted to make this more flexible and use
+ * the SOLR stream readers. But I'm pressed for time, so I cannot 
+ * spend much energy here...
+ * 
+ * This plugin will do following:
+ * 
+ * 	1. read data
+ *  2. decode data into bitsets
+ *  3. create bitset query
+ */
 public class BitSetQParserPlugin extends QParserPlugin {
 
 	public static String NAME = "bitset";
@@ -57,76 +68,10 @@ public class BitSetQParserPlugin extends QParserPlugin {
 			@Override
 			public Query parse() throws ParseException {
 				
-				/*
-				// read the data - it can be either in the content stream xor directly
-				// in the open value (perhaps encoded, compressed etc)
-
-
-
-		      Iterable<ContentStream> streams = req.getContentStreams();
-		      if (streams == null) {
-		      	// we must harvest lucene docids
-						AtomicReader reader = req.getSearcher().getAtomicReader();
-						BitSet bits = readBase64String(localParams.get(QueryParsing.V), 
-								localParams.get("compression", "none"),
-								localParams.getBool("little_endian", false));
-						
-						if (data == null) {
-		        	throw new SolrException(SolrException.ErrorCode.BAD_REQUEST, "missing data");
-		        }
-						
-		      } else {
-		        for (ContentStream stream : streams) {
-		          documentLoader.load(req, rsp, stream, processor);
-		        }
-		        if (data == null) {
-		        	throw new SolrException(SolrException.ErrorCode.BAD_REQUEST, "empty content stream");
-		        }
-		      }
-			*/
+				DataStream data = readData(req);
 				
-			// we must harvest lucene docids
-				AtomicReader reader = req.getSearcher().getAtomicReader();
-				BitSet bits = readBase64String(localParams.get(QueryParsing.V), 
-						localParams.get("compression", "none"),
-						localParams.getBool("little_endian", false));
-
-				if (bits.cardinality() < 1)
-					return null;
-
-				// TODO: add a mapper that can translate from a string 
-				// field into lucene ids
-				if (localParams.get("field", defaultField) != null) {
-					String fieldName = localParams.get("field", defaultField);
-					SchemaField field = req.getSchema().getField(fieldName);
-
-					if (field.multiValued()) {
-						throw new ParseException("I am sorry, you can't use bitset with multi-valued fields");
-					}
-
-					if (allowedFields.size() > 0 && !allowedFields.contains(fieldName)) {
-						throw new ParseException("I am sorry, you can't search against field " + fieldName + " (reason: #!@#!)");
-					}
-
-					BitSet translatedBitSet = new BitSet(reader.maxDoc());
-					int[] cache;
-					try {
-						cache = FieldCache.DEFAULT.getInts(reader, fieldName, false);
-					} catch (IOException e) {
-						throw new ParseException("Cannot get a cache for field: " + fieldName + "\n" + e.getMessage());
-					}
-					int i = 0; // lucene docid
-					for (int docValue: cache) {
-						if (bits.get(docValue)) {
-							translatedBitSet.set(i);
-						}
-						i++;
-					}
-
-					bits = translatedBitSet;
-
-				}
-
+				BitSet bits = convertDataStream(data, req);
+				
 				if (bits.cardinality() < 1)
 					return null;
 
@@ -135,9 +80,98 @@ public class BitSetQParserPlugin extends QParserPlugin {
 			}
 		};
 	}
+	
+	protected BitSet convertDataStream(DataStream data, SolrQueryRequest req) {
+		
+		bits = fromByteArray(data, isLittleEndian ? LITTLE_ENDIAN_BIT_MASK : BIG_ENDIAN_BIT_MASK);
+		
+	// we must harvest lucene docids
+		AtomicReader reader = req.getSearcher().getAtomicReader();
+		BitSet bits = readBase64String(localParams.get(QueryParsing.V), 
+				localParams.get("compression", "none"),
+				localParams.getBool("little_endian", false));
 
+		if (bits.cardinality() < 1)
+			return null;
 
-	protected BitSet readBase64String(String string, String compression, boolean isLittleEndian) 
+		// TODO: add a mapper that can translate from a string 
+		// field into lucene ids
+		if (localParams.get("field", defaultField) != null) {
+			String fieldName = localParams.get("field", defaultField);
+			SchemaField field = req.getSchema().getField(fieldName);
+
+			if (field.multiValued()) {
+				throw new ParseException("I am sorry, you can't use bitset with multi-valued fields");
+			}
+
+			if (allowedFields.size() > 0 && !allowedFields.contains(fieldName)) {
+				throw new ParseException("I am sorry, you can't search against field " + fieldName + " (reason: #!@#!)");
+			}
+
+			BitSet translatedBitSet = new BitSet(reader.maxDoc());
+			int[] cache;
+			try {
+				cache = FieldCache.DEFAULT.getInts(reader, fieldName, false);
+			} catch (IOException e) {
+				throw new ParseException("Cannot get a cache for field: " + fieldName + "\n" + e.getMessage());
+			}
+			int i = 0; // lucene docid
+			for (int docValue: cache) {
+				if (bits.get(docValue)) {
+					translatedBitSet.set(i);
+				}
+				i++;
+			}
+
+			bits = translatedBitSet;
+
+		}
+	  return null;
+  }
+
+	public static class DataStream {
+		
+	}
+
+	protected DataStream readData(SolrQueryRequest req) {
+		// read the data - it can be either in the content stream xor directly
+		// in the open value (perhaps encoded, compressed etc)
+		
+		SolrParams params = req.getParams();
+		Iterable<ContentStream> streams = req.getContentStreams();
+		byte[] data = null;
+		
+		if (streams != null) {
+			for (ContentStream stream : streams) {
+				String ct = stream.getContentType();
+				if (ct != null && ct.toLowerCase().contains("text/bitset-data")) {
+					stream.g
+				}
+			}
+			if (data == null) {
+				throw new SolrException(SolrException.ErrorCode.BAD_REQUEST, "empty content stream");
+			}
+
+		} 
+		else {
+			
+			// the data is inside the normal SOLR parameter
+			// they must always be base64 encoded (that is
+			// safety mechanism)
+			
+			AtomicReader reader = req.getSearcher().getAtomicReader();
+			data = readBase64String(params.get(QueryParsing.V), 
+					params.get("compression", "none"));
+
+			if (data == null) {
+				throw new SolrException(SolrException.ErrorCode.BAD_REQUEST, "missing data");
+			}
+		}
+		
+		return new DataStream(data, params.getBool("little_endian", false));
+  }
+
+	protected byte[] readBase64String(String string, String compression) 
 	throws ParseException {
 
 		byte[] data;
@@ -155,12 +189,17 @@ public class BitSetQParserPlugin extends QParserPlugin {
 				else if (compression.equals("zip")) {
 					data = unZip(data);
 				}
+				else if ("none".equals(compression)) {
+					// do nothing
+				}
+				else {
+					throw new ParseException("Unsupporeted compression: " + compression);
+				}
 			} catch (Exception e) {
 				throw new ParseException(e.getMessage());
 			}
 		}
-
-		return fromByteArray(data, isLittleEndian ? LITTLE_ENDIAN_BIT_MASK : BIG_ENDIAN_BIT_MASK);
+		return data;
 	}
 
 
