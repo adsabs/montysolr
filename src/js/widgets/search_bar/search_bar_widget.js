71d079702bf9349718d536da1231620930d15447
Alex <aholachek@gmail.com>
2014-04-04 15:57:50 -0400

    improved test, now has proper activate and render method
    put fonts in right place

---
+++ src/js/widgets/search_bar/search_bar_widget.js
@@ -0,0 +1,100 @@
+define(['marionette', 'hbs!./templates/search_bar_template', 'js/components/api_query', 'bootstrap'], function(Marionette, SearchBarTemplate, ApiQuery) {
+
+
+  var SearchBarView = Marionette.ItemView.extend({
+
+    template: SearchBarTemplate,
+
+    initialize: function() {},
+
+    events: {
+      "click .search-submit": "submitQuery",
+      "click .field-options span": "addField",
+    },
+
+    addField: function(e) {
+
+      var currentVal, newVal;
+
+      var df = $(e.target).attr("data-field");
+
+      if (df.split("-")[0] === "operator") {
+
+        currentVal = this.$(".q").val();
+        if (currentVal !== "") {
+          newVal = df.split("-")[1] + ":(" + currentVal + ")";
+          this.$(".q").val(newVal);
+        } else {
+          this.$(".q").val(df.split("-")[1] + ":( )");
+        }
+
+      } else {
+
+        currentVal = this.$(".q").val();
+        newVal = currentVal + " " + df + ":\"\"";
+        this.$(".q").val(newVal);
+      }
+
+    },
+
+    submitQuery: function() {
+      var query = (this.$(".q").val());
+      this.trigger("new_query", query)
+    }
+
+  })
+
+  var SearchBarWidget = Marionette.Controller.extend({
+
+    initialize: function() {
+      _.bindAll(this, "storeQuery");
+      this.view = new SearchBarView();
+    },
+
+    activate: function(beehive) {
+
+      this.pubsub = beehive.Services.get('PubSub');
+      this.key = this.pubsub.getPubSubKey();
+
+      this.afterActivate();
+
+    },
+
+    afterActivate: function() {
+
+      this.pubsub.subscribe(this.pubsub.NEW_QUERY, this.storeQuery)
+
+      this.listenTo(this.view, "new_query", function(q) {
+        this.submitNewQuery(q);
+      });
+
+    },
+
+    onClose: function() {
+      this.view.close();
+    },
+
+    storeQuery : function(apiQuery){
+      this.currentSystemQuery = apiQuery;  
+    },
+
+
+
+    render: function() {
+      return this.view.render().el;
+    },
+
+    submitNewQuery: function(query) {
+       var queryToSend = this.currentSystemQuery;
+       queryToSend.set("q", query);
+      this.pubsub.publish(this.pubsub.NEW_QUERY, queryToSend)
+    }
+  })
+
+
+  return SearchBarWidget
+
+
+
+
+})
--- test/mocha/js/widgets/search_bar_widget.spec.js
+++ test/mocha/js/widgets/search_bar_widget.spec.js
@@ -1,29 +1,68 @@
-require(['js/widgets/search_bar/search_bar_view'], function(SearchBarView, SearchBarTemplate) {
-
-    describe("Search Bar (UI Widget)", function() {
-
-        it("should render a search bar and a submit button", function() {
-
-        	var a = new SearchBarView();
-        	a.render();
-            
-        	expect(a.$(".q").length).to.equal(1);
-        	expect(a.$(".search-submit").length).to.equal(1);
-        });
-
-        it("should submit the query when the search-submit button is pressed", function(){
+define(['jquery', 'js/widgets/search_bar/search_bar_widget', 'js/components/beehive', 'js/services/pubsub', 'js/components/api_query'], function($, SearchBarWidget, BeeHive, PubSub, ApiQuery) {
 
 
+  describe("Search Bar (UI Widget)", function() {
 
-        });
+    var beehive, pubsub, key, widget, $w;
 
-        it("should notify the system of any user interaction in the query input", function(){
+    beforeEach(function(done) {
+      beehive = new BeeHive();
+      pubsub = new PubSub();
+      beehive.addService('PubSub', pubsub);
+      key = pubsub.getPubSubKey();
+
+      widget = new SearchBarWidget;
+      widget.activate(beehive.getHardenedInstance());
+      w = widget.render();
+
+      $("#test").append(w)
+
+      done();
+
+    });
+
+    afterEach(function(done) {
+      beehive, pubsub, key, widget, $w = undefined;
+      $("#test").empty();
+
+      done();
+    });
 
 
-        })
+    it("should render a search bar and a submit button", function() {
+
+      expect($(".q").length).to.equal(1);
+      expect($(".search-submit").length).to.equal(1);
+
+    });
+
+    it("should trigger a NEW_QUERY when the search-submit button is pressed", function() {
+
+      var currentSearch;
+
+      var pretendCurrentQuery = new ApiQuery({q:"star"});
+
+      pubsub.publish(key, pubsub.NEW_QUERY, pretendCurrentQuery);
+
+      pubsub.subscribe(key, pubsub.NEW_QUERY, function(apiQuery) {
+        currentSearch = apiQuery.get("q")[0]
+      });
+
+      $(".q").val("author:kurtz,m");
+      $(".search-submit").click();
+      expect(currentSearch).to.equal('author:kurtz,m');
+
+    });
 
 
+    it("should allow the user to click to add fielded search words to search bar", function() {
+
+      $("btn btn-default dropdown-toggle").click();
+      $("span[data-field=author]").click();
+      expect($(".q").val().trim()).to.equal("author:\"\"");
 
     })
+
+  });
 
 })
