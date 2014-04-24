e3591043ff5a23e21e8deb76b591291bd8f6685b
Roman Chyla <roman.chyla@gmail.com>
2014-04-23 20:43:09 -0400

    Updated docs

--- contrib/antlrqueryparser/src/java/org/apache/lucene/queryparser/flexible/aqp/parser/StandardLuceneGrammarSyntaxParser.java
+++ contrib/antlrqueryparser/src/java/org/apache/lucene/queryparser/flexible/aqp/parser/StandardLuceneGrammarSyntaxParser.java
@@ -5,7 +5,7 @@ package org.apache.lucene.queryparser.flexible.aqp.parser;
 
 /**
  * Automatically generated SyntaxParser wrapper built by ant
- * from the grammar source: /var/lib/montysolr/perpetuum/montysolr/contrib/antlrqueryparser/src/java/org/apache/lucene/queryparser/flexible/aqp/parser/StandardLuceneGrammar.g
+ * from the grammar source: /dvt/workspace/montysolr/contrib/antlrqueryparser/src/java/org/apache/lucene/queryparser/flexible/aqp/parser/StandardLuceneGrammar.g
  * 
  * YOUR CHANGES WILL BE OVERWRITTEN AUTOMATICALLY!
  */
