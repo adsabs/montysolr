6bfc9f30c46023b4e50101bbb0498b7ff94fdd77
Roman Chyla <roman.chyla@gmail.com>
2014-11-19 04:28:01 -0500

    Sorted tests alphabetically

--- test/mocha/discovery-ui2.spec.js
+++ test/mocha/discovery-ui2.spec.js
@@ -27,9 +27,7 @@ var tests = [
   //TBD 24/09/14 tbase + '/widgets/similar_widget.spec.js',
     tbase + '/widgets/tabs_widget.spec.js',
     tbase + '/widgets/wordcloud_widget.spec.js',
-    tbase + '/widgets/year_graph_facet_widget.spec.js',
-
-    tbase + '/widgets/dropdown_widget.spec.js'
+    tbase + '/widgets/year_graph_facet_widget.spec.js'
 
 
 ];
