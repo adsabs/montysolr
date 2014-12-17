e058a1714c171f7f4b4818014ea0a73a5b4807c1
Roman Chyla <roman.chyla@gmail.com>
2014-12-16 22:49:52 -0500

    Added back-to-search-results link

--- src/js/wraps/details_page_manager.js
+++ src/js/wraps/details_page_manager.js
@@ -12,7 +12,30 @@ define([
       options = options || {};
       options.template = options.template || PageManagerTemplate;
       return new PageManagerView({template: PageManagerTemplate})
+    },
+
+    activate: function (beehive) {
+      this.pubsub = beehive.getHardenedInstance().Services.get('PubSub');
+      this.debug = beehive.getDebug(); // XXX:rca - think of st better
+      this.view = this.createView({debug : this.debug, widgets: this.widgets});
+      this.pubsub.subscribe(this.pubsub.INVITING_REQUEST, _.bind(this.addQuery, this));
+    },
+
+    // xxx:rca - this is just a quick hack (the best solution would be to have
+    // the page manager re-render view (but only the non-widget parts of it)
+    addQuery: function(apiQuery) {
+      if (this.view.model)
+        this.view.model.set('query', apiQuery.url());
+    },
+
+    show: function(pageName){
+      var ret = PageManagerController.prototype.show.apply(this, arguments);
+      if (this.view.model && this.view.model.has('query')) {
+        ret.$el.find('.s-back-button-container').empty().html('<a href="#search/' + this.view.model.get('query') + '" class="back-button"> <i class="fa fa-arrow-left"></i> Back to search results</a>');
+      }
+      return ret;
     }
+
   });
   return PageManager;
 });
