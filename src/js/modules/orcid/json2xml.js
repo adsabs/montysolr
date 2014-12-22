e408f926ba5d6bd6326875087b8c55497b575ae0
Martin Obrátil <martin.obratil@gmail.com>
2014-12-22 14:26:24 +0100

    Optimization in json2xml.js

--- src/js/modules/orcid/json2xml.js
+++ src/js/modules/orcid/json2xml.js
@@ -6,28 +6,16 @@ define([
   ],
   function(_, Bootstrap, GenericModule, Mixins) {
 
-    // BC:rca - I don't want global exports (unless it is required by json2xml - and i'd be very suspicious of
-    // that library, if it was); please move to a standalone function
-    // TODO: move this to some commonUtils.js
-    String.prototype.format = function () {
-      var args = arguments;
-      return this.replace(/\{\{|\}\}|\{(\d+)\}/g, function (m, n) {
-        if (m == "{{") {
-          return "{";
-        }
-        if (m == "}}") {
-          return "}";
-        }
-        return args[n];
-      });
-    };
-
     var settings = {
       attributes_key:false,
       header:false
     };
 
     var xml = function(json, opts) {
+      return _.flatten(_toArrayOfXmls(json, opts)).join("");
+    };
+
+    var _toArrayOfXmls = function(json, opts) {
       if(opts){
         Object.keys(settings).forEach(function(k){
           if(typeof opts[k]==='undefined'){
@@ -38,7 +26,9 @@ define([
         opts = settings;
       }
 
-      var result = opts.header?'<?xml version="1.0" encoding="UTF-8"?>':'';
+      var result = [];
+      result.push(opts.header ? '<?xml version="1.0" encoding="UTF-8"?>' : '');
+
       opts.header = false;
       type = json.constructor.name;
 
@@ -46,43 +36,57 @@ define([
 
         result = [];
         json.forEach(function(node){
-          result.push(xml(node, opts));
+          result.push(_toArrayOfXmls(node, opts));
         });
-
       } else if(type ==='Object' && typeof json === "object") {
 
         Object.keys(json).forEach(function(key){
           if(key!==opts.attributes_key){
             var node = json[key],
-              attributes = '';
+              attributes = [];
 
             if(opts.attributes_key && node[opts.attributes_key]){
               Object.keys(node[opts.attributes_key]).forEach(function(k){
-                attributes += ' {0}="{1}"'.format(k, node[opts.attributes_key][k]);
+                attributes.push([' ',
+                                k,
+                                '="',
+                                node[opts.attributes_key][k],
+                                '"'])
               });
             }
-            var inner = xml(node,opts);
+            var inner = _toArrayOfXmls(node,opts);
 
             if(inner){
               if (key == "_") {
-                result += node;
+                result.push(node);
               }
               else {
-                var next = xml(node, opts);
+                var next = _toArrayOfXmls(node, opts);
 
-                next = Array.isArray(next) ? next : [next];
+                next = node.constructor.name === "Array" ? next : [next];
 
                 next.forEach(function(item) {
-                  result += "<{0}{1}>{2}</{3}>".format(key, attributes, item, key);
+                 result.push(["<",
+                              key,
+                              attributes,
+                              ">",
+                              item,
+                              "</",
+                              key,
+                              ">"]);
                 });
               }
             } else {
-              result += "<{0}{1}/>".format(key, attributes);
+              result.push(
+                ["<",
+                  key,
+                  attributes,
+                  "/>"]);
             }
           }
         });
       } else {
-        return json.toString().replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+        result.push(json.toString().replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;'));
       }
 
       return result;
