dc424878121e15a5ee031c0dafb94ee9a40f313f
Roman Chyla <roman.chyla@gmail.com>
2015-05-05 15:57:06 -0400

    Lowering the coverage limit to the original 40%

--- Gruntfile.js
+++ Gruntfile.js
@@ -455,7 +455,7 @@ module.exports = function(grunt) {
           moduleThreshold : 80,
           modulePattern : "../../js/(.*)",
           customModuleThreshold: {
-            "apps/discovery/router.js": 48,
+            "apps/discovery/router.js": 40,
             "widgets/facet/graph-facet/h_index_graph.js":2,
             "widgets/facet/graph-facet/year_graph.js":2,
             "wraps/graph_tabs.js":5,
