ec6d95f6674f07e2cec30372dc2f3207333db222
Alex <aholachek@gmail.com>
2015-11-11 16:00:03 -0500

    more orcid additions

---
+++ src/js/wraps/orcid_page_manager/orcid_page_manager.js
@@ -0,0 +1,21 @@
+define([
+  'js/page_managers/controller',
+  'js/page_managers/one_column_view',
+  'hbs!./orcid-page-layout'
+], function (
+  PageManagerController,
+  PageManagerView,
+  PageManagerTemplate) {
+
+  var PageManager = PageManagerController.extend({
+
+    createView: function(options) {
+      options = options || {};
+      options.template = PageManagerTemplate;
+      return new PageManagerView({template: PageManagerTemplate, className :  "s-orcid-layout",  id : "orcid-page-layout"  })
+    }
+  });
+
+  return PageManager;
+
+});
