3980df0f620ea0e056e2630fd2bea7a633abe404
Edwin Henneken <ehenneken@gmail.com>
2016-02-03 12:22:28 -0500

    Implemented proper handling on PubSub communication

--- src/js/wraps/object_facet.js
+++ src/js/wraps/object_facet.js
@@ -21,82 +21,91 @@ define([
   ) {
 
   return function() {
-    var widget = FacetFactory.makeHierarchicalCheckboxFacet({
-      facetField: "simbad_object_facet_hier",
-      facetTitle: "SIMBAD Objects",
-      openByDefault: true,
-      logicOptions: {single: ['limit to', 'exclude'], 'multiple': ['and', 'or', 'exclude']},
-      responseProcessors: [
-        function (v) {
-          var vv = v.split('/');
-          return vv[vv.length - 1];
-        }
-      ]
-    });
-	widget._getNewCache = function(options) {
-	  	return new Cache(_.extend({
-	  		'maximumSize': 150,
-			'expiresAfterWrite':60*30 // 30 mins
-	  	}, _.isObject(options) ? options : {}));
-	};
-	widget.processFacetResponse = function(apiResponse, data) {
-	    if (!('attributes' in apiResponse)) {
-			console.log("processing Solr data");
-			// We received a Solr response, so we need to do the following:
-			// 1. Leave the top level object facet entries untouched (like "0/Galaxy"),
-			//    and for deeper entries (like "1/Galaxy/<identifier>"), extract the identifiers
-			// 2. Send the list of identifiers to the object service end point
-			// 3. For the deeper entries, replace the identifiers with the canonical object names
-			var info = this.registerResponse(apiResponse, data);
-	        var facets = this.extractFacets(apiResponse);
-	        // no data for us
-	        if (!facets) {
-	          console.warn('No facet data for:', this.facetField);
-	          return;
-	        }
-	        // construct a list of SIMBAD identifiers
-			var ident = facets.filter(function(v){return (typeof v === 'string');}).filter(function(v){return (v.charAt(0)==='1');});
-			//
-	        // For non-empty lists of facet strings, translate the SIMBAD ids
-			if (ident.length > 0) {
-				var identifiers = ident.map(function(v){
-					var vv = v.split("/");
-					return vv[vv.length-1]
-				});
-				this.getSIMBADobjects(identifiers);
-			};
-	        var facetCollection = this.processFacets(apiResponse, facets);
-			var updater = function(facet) {
-	            var v = facet.value;
-				var objId;
-				if (v.charAt(0)==='1') {
-					var vv = v.split("/");
-					var objId = vv[vv.length-1];
-				}
-				var title = facet.title;
-				var oname = widget._cache.getSync(objId);
-				if (objId && objId === title && oname) {
-				 	facet.title = oname;
-				};			 
-			};
-			console.log(widget._cache);
-			if (!(typeof widget._cache === 'undefined')) {
-				facetCollection.forEach(updater);
-			}
-	        this.updateCollectionAndView(info, facetCollection);
-	  	} else {
-			console.log("processing micro service data");
-			// We received a response from the micro service, so we need to do the following:
-			// 1. Add the mapping from the numerical object idetifier to the canonical object nane
-			if (typeof widget._cache === 'undefined') {
-				widget._cache = widget._getNewCache();
-			}
-			for (var objId in apiResponse.attributes) {
-				widget._cache.put(objId, apiResponse.attributes[objId]['canonical']);
-			};
-		}
-	};
+	  var widget = FacetFactory.makeHierarchicalCheckboxFacet({
+		  facetField: "simbad_object_facet_hier",
+		  facetTitle: "SIMBAD Objects",
+		  openByDefault: true,
+		  logicOptions: {single: ['limit to', 'exclude'], 'multiple': ['and', 'or', 'exclude']},
+		  responseProcessors: [
+			  function (v) {
+				  var vv = v.split('/');
+				  return vv[vv.length - 1];
+			  }
+		  ]
+	  });
+
+	  widget._getNewCache = function(options) {
+		  return new Cache(_.extend({
+			  'maximumSize': 150,
+			  'expiresAfterWrite':60*30 // 30 mins
+		  }, _.isObject(options) ? options : {}));
+	  };
+	  widget.processFacetResponse = function(apiResponse, data) {
+		  // We received a response from the micro service, so we need to do the following:
+		  // 1. Add the mapping from the numerical object idetifier to the canonical object nane
+		  if (typeof this._cache === 'undefined') {
+			  this._cache = this._getNewCache();
+		  }
+          // Now we look at what kind of response was submitted
+		  if (!('attributes' in apiResponse)) {
+			  // We received a Solr response, so we need to do the following:
+			  // 1. Leave the top level object facet entries untouched (like "0/Galaxy"),
+			  //    and for deeper entries (like "1/Galaxy/<identifier>"), extract the identifiers
+			  // 2. Send the list of identifiers to the object service end point
+			  // 3. For the deeper entries, replace the identifiers with the canonical object names
+			  var info = this.registerResponse(apiResponse, data);
+			  var facets = this.extractFacets(apiResponse);
+			  // no data for us
+			  if (!facets) {
+				  console.warn('No facet data for:', this.facetField);
+				  return;
+			  }
+			  // construct a list of SIMBAD identifiers
+			  var ident = facets.filter(function(v){return (typeof v === 'string');}).filter(function(v){return (v.charAt(0)==='1');});
+			  //
+			  // For non-empty lists of facet strings, translate the SIMBAD ids
+			  if (ident.length > 0) {
+				  var identifiers = ident.map(function(v){
+					  var vv = v.split("/");
+					  var identifier = vv[vv.length-1];
+//					  var seen_before = widget._cache.getSync(identifier);
+//					  if (!seen_before) {
+//					  	return identifier
+//					  }
+					  return identifier
+				  });
+				  this.getSIMBADobjects(identifiers);
+			  };
+			  var facetCollection = this.processFacets(apiResponse, facets);
+			  var updater = function(facet) {
+				  var v = facet.value;
+				  var objId;
+				  if (v.charAt(0)==='1') {
+					  var vv = v.split("/");
+					  var objId = vv[vv.length-1];
+				  }
+				  var title = facet.title;
+				  var oname = widget._cache.getSync(objId);
+				  if (objId && objId === title && oname) {
+					  facet.title = oname;
+				  };
+			  };
+			  facetCollection.forEach(updater);
+			  this.updateCollectionAndView(info, facetCollection);
+		  } else {
+			  // The response was a micro service response
+			  // Every entry provides a translation from SIMBAD object ID to
+			  // canonical object name
+			  for (var objId in apiResponse.attributes) {
+				  widget._cache.put(objId, apiResponse.attributes[objId]['canonical']);
+			  };
+		  }
+	  };
+
     widget.getSIMBADobjects = function (identifiers) {
+		// This method provides a call to the object search micro service
+		// Given a list of SIMBAD object identifiers (integers), it will return
+		// a mapping, associating every identifier with a canonical object name
 		var pubsub = this.getPubSub();
         var request =  new ApiRequest({
           target: ApiTargets.SERVICE_OBJECTS,
@@ -106,8 +115,11 @@ define([
             contentType : "application/json"
 		  }
         });
-		pubsub.subscribe(pubsub.DELIVERING_RESPONSE, this.processFacetResponse);
+		// Register a callback for this particular query
+		this.registerCallback(request.get('query').url(), this.processFacetResponse);
+		// Publish the micro service query to the PubSUb
 		pubsub.publish(pubsub.EXECUTE_REQUEST, request);
+
     };
     return widget;
   };
--- test/mocha/js/widgets/object_facet.spec.js
+++ test/mocha/js/widgets/object_facet.spec.js
@@ -10,7 +10,10 @@ define([
     'js/bugutils/minimal_pubsub',
 	'js/widgets/facet/factory',
     'test/mocha/js/widgets/test_json/test_SIMBAD',
-	'test/mocha/js/widgets/test_json/test_object_service_data'
+	'test/mocha/js/widgets/test_json/test_object_service_data',
+	'js/widgets/base/paginated_multi_callback_widget',
+	'js/components/api_query_updater',
+		'js/wraps/object_facet'
   ],
   function(
 	       _,
@@ -24,7 +27,10 @@ define([
            MinimalPubSub,
 	       FacetFactory,
            SolrData,
-	       ObjServiceData
+	       ObjServiceData,
+	       PaginatedMultiCallbackWidget,
+	       ApiQueryUpdater,
+		   ObjectFacet
     ) {
 
     describe("Object Facet Widget (object_facet.spec.js)", function () {
@@ -34,10 +40,11 @@ define([
         this.minsub = minsub = new (MinimalPubSub.extend({
           request: function (request) {
 			  if (request.get("target") == "search/query"){
-				  console.log("Getting Solr data");
+				  // When we're called as a Solr search, we return Solr stub data
 				  return SolrData();
 			  } else {
-				  console.log("Getting micro service data");
+				  // In all other cases we return stub data representing a response 
+				  // from the object search micro service
 				  return ObjServiceData();
 			  }
           }
@@ -50,95 +57,23 @@ define([
       });
 	  */
 
-      it("knows to listen to the pubsub", function() {
-	    var widget = FacetFactory.makeHierarchicalCheckboxFacet({
-	        facetField: "simbad_object_facet_hier",
-	        facetTitle: "SIMBAD Objects",
-	        openByDefault: true,
-	        logicOptions: {single: ['limit to', 'exclude'], 'multiple': ['and', 'or', 'exclude']},
-	        responseProcessors: [
-	          function (v) {
-	            var vv = v.split('/');
-	            return vv[vv.length - 1];
-	          }
-	        ]
-	    });
-		widget._getNewCache = function(options) {
-		  	return new Cache(_.extend({
-		  		'maximumSize': 150,
-				'expiresAfterWrite':60*30 // 30 mins
-		  	}, _.isObject(options) ? options : {}));
-		};
-		widget.processFacetResponse = function(apiResponse, data) {
-		    if (!('attributes' in apiResponse)) {
-				console.log("processing Solr data");
-				// We received a Solr response, so we need to do the following:
-				// 1. Leave the top level object facet entries untouched (like "0/Galaxy"),
-				//    and for deeper entries (like "1/Galaxy/<identifier>"), extract the identifiers
-				// 2. Send the list of identifiers to the object service end point
-				// 3. For the deeper entries, replace the identifiers with the canonical object names
-				var info = this.registerResponse(apiResponse, data);
-		        var facets = this.extractFacets(apiResponse);
-		        // no data for us
-		        if (!facets) {
-		          console.warn('No facet data for:', this.facetField);
-		          return;
-		        }
-		        // construct a list of SIMBAD identifiers
-				var ident = facets.filter(function(v){return (typeof v === 'string');}).filter(function(v){return (v.charAt(0)==='1');});
-				//
-		        // For non-empty lists of facet strings, translate the SIMBAD ids
-				if (ident.length > 0) {
-					var identifiers = ident.map(function(v){
-						var vv = v.split("/");
-						return vv[vv.length-1]
-					});
-					this.getSIMBADobjects(identifiers);
-				};
-		        var facetCollection = this.processFacets(apiResponse, facets);
-				var updater = function(facet) {
-		            var v = facet.value;
-					var objId;
-					if (v.charAt(0)==='1') {
-						var vv = v.split("/");
-						var objId = vv[vv.length-1];
-					}
-					var title = facet.title;
-					var oname = widget._cache.getSync(objId);
-					if (objId && objId === title && oname) {
-					 	facet.title = oname;
-					};			 
-				};
-				facetCollection.forEach(updater);
-		        this.updateCollectionAndView(info, facetCollection);
-		  	} else {
-				console.log("processing micro service data");
-				// We received a response from the micro service, so we need to do the following:
-				// 1. Add the mapping from the numerical object idetifier to the canonical object nane
-				if (typeof this._cache === 'undefined') {
-					widget._cache = widget._getNewCache();
-				} else {
-					console.log("cache already exists");
-				}
-				for (var objId in apiResponse.attributes) {
-					widget._cache.put(objId, apiResponse.attributes[objId]['canonical']);
-				};
-			}
-		};
-	    widget.getSIMBADobjects = function (identifiers) {
-			var request =  new ApiRequest();
-			minsub.subscribeOnce(minsub.DELIVERING_RESPONSE, this.processFacetResponse);
-			minsub.publish(minsub.EXECUTE_REQUEST, request);
-	    };
+      it("Gets object facet data from Solr docs and translates the SIMBAD object IDs", function() {
+		// Create a new SIMBAD Objects facet
+        var widget = new ObjectFacet();
 
         widget.activate(this.minsub.beehive.getHardenedInstance());
 
 		var $w = $(widget.render().el);
         $('#test-area').append($w);
 		window.$w = $w;
-
+        // Initiate a search
         minsub.publish(minsub.START_SEARCH, minsub.createQuery({'q': 'foo'}));
-
+        // This results in
+		// 1. The initial search query results in Solr stub data being sent to the object facet
+		// 2. The hierarchical object facet data in the Solr stub data initiates a call to
+		//    the SIMBAD micro service which, through a "call back", results in micro service
+		//    stub data being sent back, which will get cached (SIMBAD id as key, SIMBAD object
+		//    name as value)
         // We now should have a facet called "SIMBAD Objects"
 		expect($("#test-area .widget-name h3").text().trim()).to.eql("SIMBAD Objects")
 		// We expect a top level entry "X-ray" with 1 correspnding record
