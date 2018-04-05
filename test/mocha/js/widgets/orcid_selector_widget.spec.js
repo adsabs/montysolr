50adfeeada2ca26bb87d144eaf42a4e5ebc41cc6
Tim Hostetler <timothy.hostetler@cfa.harvard.edu>
2018-04-05 00:56:14 -0400

    Fix orcid-selector tests

--- test/mocha/js/widgets/orcid_selector_widget.spec.js
+++ test/mocha/js/widgets/orcid_selector_widget.spec.js
@@ -4,52 +4,55 @@ define([
   'es6!js/widgets/orcid-selector/widget.jsx'
 ], function (_, MinPubSub, Widget) {
 
-  var before = function () {
+  var init = function () {
     this.sb = sinon.sandbox.create();
-    this.w = new Widget();
-    this.minsub = new MinPubSub();
-    this.pubsub = this.minsub.pubsub.getHardenedInstance();
-    this.w.activate(this.minsub.beehive.getHardenedInstance());
-    this.state = _.bind(function () {
-      return this.w.store.getState().get('OrcidSelectorApp');
-    }, this);
+    this.pubsub = new (MinPubSub.extend({
+      request: _.identity
+    }))({ verbose: false });
+    this.state = function (w) {
+      return w.store.getState().get('OrcidSelectorApp');
+    };
   };
 
-  var after = function () {
+  var teardown = function () {
     this.sb.restore();
-    this.w.destroy();
-    this.w = null;
+    this.state = null;
+    $('#test-area').empty();
   };
 
   describe('Orcid Selector Widget (orcid_selector_widget.spec.js)', function () {
-    beforeEach(before);
-    afterEach(after);
+    beforeEach(init);
+    afterEach(teardown);
 
     it('Does not render if orcidModeIsOn is false', function () {
+      const w = new Widget();
+      w.activate(this.pubsub.beehive);
 
       // check to make sure mode is off
-      expect(this.state().get('mode')).to.equal(false);
+      expect(this.state(w).get('mode')).to.equal(false);
 
       // render and see that it's empty
-      expect(this.w.view.render().$el.children().length).to.equal(0);
+      expect(w.view.render().$el.children().length).to.equal(0);
     });
 
     it('Does render if orcidModeIsOn is true', function () {
-      this.minsub.beehive.addObject('User', { isOrcidModeOn: _.constant(true) });
-
-      this.w.activate(this.minsub.beehive);
+      const w = new Widget();
+      const beehive = this.pubsub.beehive;
+      beehive.addObject('User', { isOrcidModeOn: _.constant(true) });
+      w.activate(beehive);
 
       // check to make sure mode is on
-      expect(this.state().get('mode')).to.equal(true);
+      expect(this.state(w).get('mode')).to.equal(true);
 
       // render and see that it's empty
-      expect(this.w.view.render().$el.children().length).to.be.gt(0);
+      expect(w.view.render().$el.children().length).to.be.gt(0);
     });
 
     it('State is updated correctly when mode is changed', function () {
-
+      const w = new Widget();
+      w.activate(this.pubsub.beehive);
       // should be off initially
-      expect(this.state().get('mode')).to.equal(false);
+      expect(this.state(w).get('mode')).to.equal(false);
 
       // trigger a user event
       this.pubsub.publish(this.pubsub.USER_ANNOUNCEMENT, '', {
@@ -57,34 +60,37 @@ define([
       });
 
       // should now be on
-      expect(this.state().get('mode')).to.equal(true);
+      expect(this.state(w).get('mode')).to.equal(true);
     });
 
     it('State is update correctly when number of papers is changed', function () {
-      this.minsub.beehive.addObject('AppStorage', {
+      const w = new Widget();
+      const beehive = this.pubsub.beehive;
+      beehive.addObject('AppStorage', {
         getSelectedPapers: function () { return ['foo', 'bar']; }
       });
+      w.activate(beehive);
 
-      this.w.activate(this.minsub.beehive);
-
-      expect(this.state().get('selected').toJS().length).to.equal(0);
+      expect(this.state(w).get('selected').toJS().length).to.equal(0);
 
       this.pubsub.publish(this.pubsub.STORAGE_PAPER_UPDATE);
 
-      expect(this.state().get('selected').length).to.equal(2);
+      expect(this.state(w).get('selected').length).to.equal(2);
     });
 
     it('Clicking claim button publishes the proper claim event', function () {
-      this.minsub.beehive.addObject('User', { isOrcidModeOn: _.constant(true) });
-      this.minsub.beehive.addObject('AppStorage', {
+      const w = new Widget();
+      const beehive = this.pubsub.beehive;
+      beehive.addObject('User', { isOrcidModeOn: _.constant(true) });
+      beehive.addObject('AppStorage', {
         getSelectedPapers: function () { return ['foo', 'bar']; }
       });
-      this.w.activate(this.minsub.beehive);
+      w.activate(beehive);
 
       // render and grab element
-      var $el = this.w.view.$el;
+      var $el = w.view.$el;
       $('#test-area').append($el);
-      this.w.view.render();
+      w.view.render();
 
       // create subscription
       var spy = this.sb.spy();
@@ -97,20 +103,22 @@ define([
       $('button:contains("Apply")', $el).click();
 
       expect(spy.args[0][0]).is.equal('orcid-bulk-claim');
-      expect(spy.args[0][1]).is.equal(this.state().get('selected'));
+      expect(spy.args[0][1]).is.equal(this.state(w).get('selected'));
     });
 
     it('Clicking delete button publishes the proper delete event', function () {
-      this.minsub.beehive.addObject('User', { isOrcidModeOn: _.constant(true) });
-      this.minsub.beehive.addObject('AppStorage', {
+      const w = new Widget();
+      const beehive = this.pubsub.beehive;
+      beehive.addObject('User', { isOrcidModeOn: _.constant(true) });
+      beehive.addObject('AppStorage', {
         getSelectedPapers: function () { return ['foo', 'bar']; }
       });
-      this.w.activate(this.minsub.beehive);
+      w.activate(beehive);
 
       // render and grab hold of the element
-      var $el = this.w.view.$el;
+      var $el = w.view.$el;
       $('#test-area').append($el);
-      this.w.view.render();
+      w.view.render();
 
       // add a subscription
       var spy = this.sb.spy();
@@ -123,9 +131,7 @@ define([
       $('button:contains("Apply")', $el).click();
 
       expect(spy.args[0][0]).is.equal('orcid-bulk-delete');
-      expect(spy.args[0][1]).is.equal(this.state().get('selected'));
+      expect(spy.args[0][1]).is.equal(this.state(w).get('selected'));
     });
-
   });
-
 });
