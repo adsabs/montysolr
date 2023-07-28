01ee5af0b34b96d6e952a970cb2f05ba43e1283f
Jean-Claude Paquin <jean-claude.paquin@cfa.harvard.edu>
2023-07-28 19:16:18 -0400

    Remove more unused test scaffolding

--- montysolr/src/test/java/org/apache/solr/MontySolrTestCaseJ4.java
+++ montysolr/src/test/java/org/apache/solr/MontySolrTestCaseJ4.java
@@ -10,26 +10,6 @@ import java.net.URLClassLoader;
 
 public class MontySolrTestCaseJ4 extends SolrTestCaseJ4 {
 
-
-    public static String TEST_HOME() {
-        return MontySolrSetup.getSolrHome();
-    }
-
-    public static String MONTYSOLR_HOME = MontySolrSetup.getMontySolrHome();
-    private static final String SOLR_HOME = MontySolrSetup.getSolrHome();
-    private static final String SOURCE_HOME = MONTYSOLR_HOME
-            + "/src/test-files";
-    public static String TEST_HOME = SOURCE_HOME + "/solr";
-    public static String WEBAPP_HOME = new File(SOLR_HOME, "src/webapp/web")
-            .getAbsolutePath();
-    public static String EXAMPLE_HOME = new File(SOLR_HOME, "example/solr")
-            .getAbsolutePath();
-    public static String EXAMPLE_MULTICORE_HOME = new File(SOLR_HOME,
-            "example/multicore").getAbsolutePath();
-    public static String EXAMPLE_SCHEMA = EXAMPLE_HOME + "/conf/schema.xml";
-    public static String EXAMPLE_CONFIG = EXAMPLE_HOME + "/conf/solrconfig.xml";
-
-
     /**
      * A trick to add resources to a classpath so that we can run
      * tests inside the development dir, but not necessarily install
