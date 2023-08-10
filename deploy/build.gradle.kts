129a7823d56476472ede3019fced1984f36a4f1e
Jean-Claude Paquin <jean-claude.paquin@cfa.harvard.edu>
2023-08-10 02:32:57 -0400

    Copy dependencies to an accessible location

--- deploy/build.gradle.kts
+++ deploy/build.gradle.kts
@@ -2,6 +2,27 @@ plugins {
 	java
 }
 
+repositories {
+	mavenCentral()
+
+	// Required for Solr restlet dependency
+	maven {
+		url = uri("https://maven.restlet.talend.com")
+	}
+}
+
 dependencies {
 	implementation(project(":montysolr"))
+}
+
+// Copy libs to build/libs so they can be added to solr's lib path later
+tasks.register<Copy>("copyDependenciesToLib") {
+    into("$buildDir/libs")
+    from(project.configurations.runtimeClasspath)
+}
+
+tasks {
+	build {
+		dependsOn("copyDependenciesToLib")
+	}
 }
